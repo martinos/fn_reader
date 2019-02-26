2a793e369c928957c98700a1f07826e976559358
Martin Chabot <chabotm@gmail.com>
2019-02-26 11:53:35 -0500

    Adding tests

--- lib/fn_reader.rb
+++ lib/fn_reader.rb
@@ -1,6 +1,26 @@
 require "fn_reader/version"
 
 module FnReader
-  class Error < StandardError; end
-  # Your code goes here...
+  class NameError < StandardError; end
+end
+
+class Module
+  # stolen from Rails mattr_accessor
+  def fn_reader(*syms)
+    syms.each do |sym|
+      raise FnReader::NameError.new("invalid attribute name: #{sym}") unless (sym =~ /\A[_A-Za-z]\w*\z/)
+      class_eval(<<-EOS, __FILE__, __LINE__ + 1)
+        @@#{sym} = nil unless defined? @@#{sym}
+        def self.#{sym}
+          @@#{sym}
+        end
+      EOS
+
+      class_eval(<<-EOS, __FILE__, __LINE__ + 1)
+          def #{sym}
+            @@#{sym}
+          end
+        EOS
+    end
+  end
 end
--- test/fn_reader_test.rb
+++ test/fn_reader_test.rb
@@ -1,11 +1,22 @@
 require "test_helper"
 
+module MyFns
+  fn_reader :id
+  @@id = -> a { a }
+end
+
 class FnReaderTest < Minitest::Test
+  include MyFns
+
   def test_that_it_has_a_version_number
     refute_nil ::FnReader::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_included_module
+    assert_equal 1, id.(1)
+  end
+
+  def test_module_function
+    assert_equal 1, MyFns.id.(1)
   end
 end
